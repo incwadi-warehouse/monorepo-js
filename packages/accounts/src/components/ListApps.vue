0263c7eb163f4eab72a8bfbe3a8ec614acb7d60d
André Baldeweg <a@baldeweg.org>
2022-09-14 14:56:16 +0000

    Add ListApps component

---
+++ packages/accounts/src/components/ListApps.vue
@@ -0,0 +1,19 @@
+<script setup>
+const catalog = import.meta.env.VUE_APP_CATALOG
+const settings = import.meta.env.VUE_APP_SETTINGS
+const orders = import.meta.env.VUE_APP_ORDERS
+</script>
+
+<template>
+  <ul>
+    <li>
+      <a :href="catalog">{{ $t('catalog') }}</a>
+    </li>
+    <li>
+      <a :href="settings">{{ $t('settings') }}</a>
+    </li>
+    <li>
+      <a :href="orders">{{ $t('orders') }}</a>
+    </li>
+  </ul>
+</template>
--- packages/accounts/src/views/IndexView.vue
+++ packages/accounts/src/views/IndexView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useTitle } from '@baldeweg/ui'
 import { useI18n } from 'vue-i18n'
+import ListApps from '@/components/ListApps.vue'
 import { useAuth } from '@/composables/useAuth.js'
 import LoginForm from '@/components/LoginForm.vue'
 import ChangePassword from '@/components/ChangePassword.vue'
@@ -10,10 +11,6 @@ const { t } = useI18n()
 useTitle({ title: t('login') })
 
 const { isAuthenticated, user, logout } = useAuth()
-
-const catalog = import.meta.env.VUE_APP_CATALOG
-const settings = import.meta.env.VUE_APP_SETTINGS
-const orders = import.meta.env.VUE_APP_ORDERS
 </script>
 
 <template>
@@ -28,17 +25,7 @@ const orders = import.meta.env.VUE_APP_ORDERS
   </b-container>
 
   <b-container size="s" v-if="isAuthenticated && user">
-    <ul>
-      <li>
-        <a :href="catalog">{{ $t('catalog') }}</a>
-      </li>
-      <li>
-        <a :href="settings">{{ $t('settings') }}</a>
-      </li>
-      <li>
-        <a :href="orders">{{ $t('orders') }}</a>
-      </li>
-    </ul>
+    <ListApps />
   </b-container>
 
   <b-container size="s" v-if="!isAuthenticated">
