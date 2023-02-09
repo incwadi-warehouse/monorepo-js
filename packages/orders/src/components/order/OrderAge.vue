b78320162b0835eb710b3e034ff758eb9128ab94
André Baldeweg <a@baldeweg.org>
2023-02-09 09:54:11 +0000

    Refactor detail view

---
+++ packages/orders/src/components/order/OrderAge.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from 'vue'
+import dayjs from 'dayjs'
+
+const props = defineProps({
+  created: Number,
+})
+
+const diff = computed(() => {
+  const duration = dayjs().diff(dayjs.unix(props.created))
+
+  return Math.round(duration / 1000 / 86400)
+})
+</script>
+
+<template>
+  <BContainer size="m" v-if="diff > 14">
+    <BAlert type="error">
+      {{ $t('old_reservation', { days: diff }) }}
+    </BAlert>
+  </BContainer>
+</template>
---
+++ packages/orders/src/components/order/OrderCustomer.vue
@@ -0,0 +1,129 @@
+<script setup>
+defineProps({
+  reservation: Object,
+})
+</script>
+
+<template>
+  <BContainer size="m">
+    <h2>{{ $t('customer') }}</h2>
+
+    <details>
+      <summary class="selector">
+        {{ $t('customer_details') }}
+      </summary>
+
+      <BForm @submit.prevent="$emit('update')">
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="salutation">
+              {{ $t('salutation') }}
+            </BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormSelect
+              :items="[
+                { key: 'm', value: $t('mr') },
+                { key: 'f', value: $t('mrs') },
+                { key: 'd', value: $t('none_divers') },
+              ]"
+              id="salutation"
+              :modelValue="reservation.salutation"
+              @update:modelValue="$emit('update:salutation', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="firstname">
+              {{ $t('firstname') }}
+            </BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormInput
+              type="text"
+              id="firstname"
+              :modelValue="reservation.firstname"
+              @update:modelValue="$emit('update:firstname', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="surname">
+              {{ $t('surname') }}
+            </BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormInput
+              type="text"
+              id="surname"
+              :modelValue="reservation.surname"
+              @update:modelValue="$emit('update:surname', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="mail">
+              {{ $t('mail') }}
+            </BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormInput
+              type="email"
+              id="mail"
+              :modelValue="reservation.mail"
+              @update:modelValue="$emit('update:mail', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="phone">
+              {{ $t('phone') }}
+            </BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BFormInput
+              type="tel"
+              id="phone"
+              :modelValue="reservation.phone"
+              @update:modelValue="$emit('update:phone', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup>
+          <BFormItem>
+            <BFormLabel for="notes">{{ $t('notes') }}</BFormLabel>
+          </BFormItem>
+          <BFormItem>
+            <BForm-textarea
+              id="notes"
+              :modelValue="reservation.notes"
+              @update:modelValue="$emit('update:notes', $event)"
+            />
+          </BFormItem>
+        </BFormGroup>
+
+        <BFormGroup buttons>
+          <BFormItem>
+            <b-button design="outline">{{ $t('save') }}</b-button>
+          </BFormItem>
+        </BFormGroup>
+      </BForm>
+    </details>
+  </BContainer>
+</template>
+
+<style scoped>
+.selector {
+  cursor: pointer;
+  user-select: none;
+}
+</style>
