387b07ab62269e9ce3babb1adc876823d4aef2cc
André Baldeweg <a@baldeweg.org>
2023-03-02 15:05:59 +0000

    Sell all products from order

---
+++ packages/orders/src/composables/useProduct.js
@@ -0,0 +1,23 @@
+import { useRequest } from '@baldeweg/ui'
+import Cookies from 'js-cookie'
+
+export function useProduct() {
+  const { config, setAuthHeader, request } = useRequest()
+
+  config.value.baseURL = import.meta.env.VUE_APP_API
+  setAuthHeader(Cookies.get('token'))
+
+  const sellAll = (products) => {
+    products.forEach((element) => {
+      sell(element.id)
+    })
+  }
+
+  const sell = (id) => {
+    return request('put', '/api/book/sell/' + id)
+  }
+
+  return {
+    sellAll,
+  }
+}
