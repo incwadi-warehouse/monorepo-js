8233101416c5cd4c4d24070c3be4ecd4002bd2af
André <a@baldeweg.org>
2023-04-15 16:44:16 +0000

    Fix test

--- packages/settings/cypress/e2e/settings.cy.js
+++ packages/settings/cypress/e2e/settings.cy.js
@@ -3,6 +3,10 @@ describe('settings', () => {
     cy.server()
     cy.route('GET', '**/api/me', {})
 
+    cy.route('GET', '**/api/user/1/snow', {
+      value: false,
+    })
+
     cy.setCookie('token', 'token')
     cy.setCookie('refresh_token', 'refresh_token')
 
