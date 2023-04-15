f3f87b7ec0f3f6c8eb30047fc7760137697f0b3a
André <a@baldeweg.org>
2023-04-15 21:15:02 +0000

    Improve mock handler

--- packages/accounts/src/mocks/handlers.js
+++ packages/accounts/src/mocks/handlers.js
@@ -1,14 +1,25 @@
 import { rest } from 'msw'
 
-const optionResponse = (req, res, ctx) => {
-  return res(
-    ctx.status(200),
-    ctx.json({
-      value: JSON.parse(localStorage.getItem('mock_' + req.params.key)),
-    })
-  )
-}
-
 export const handlers = [
-  rest.get('http://localhost:8080/api/user/1/:key', optionResponse),
+  rest.get('http://localhost:8080/api/user/1/:key', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        value: JSON.parse(localStorage.getItem('mock_' + req.params.key)),
+      })
+    )
+  }),
+  rest.post('http://localhost:8080/api/user/1/:key', async (req, res, ctx) => {
+    const content = await req.json()
+    JSON.stringify(
+      localStorage.setItem('mock_' + req.params.key, content.value)
+    )
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        value: JSON.parse(localStorage.getItem('mock_' + req.params.key)),
+      })
+    )
+  }),
 ]
--- packages/orders/src/mocks/handlers.js
+++ packages/orders/src/mocks/handlers.js
@@ -1,14 +1,12 @@
 import { rest } from 'msw'
 
-const optionResponse = (req, res, ctx) => {
-  return res(
-    ctx.status(200),
-    ctx.json({
-      value: JSON.parse(localStorage.getItem('mock_' + req.params.key)),
-    })
-  )
-}
-
 export const handlers = [
-  rest.get('http://localhost:8080/api/user/1/:key', optionResponse),
+  rest.get('http://localhost:8080/api/user/1/:key', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        value: JSON.parse(localStorage.getItem('mock_' + req.params.key)),
+      })
+    )
+  }),
 ]
