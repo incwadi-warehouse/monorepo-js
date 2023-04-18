ed2b34f395700b24dadad3c85861abbdf7b56d91
André <a@baldeweg.org>
2023-04-18 11:23:03 +0000

    Add tests

--- packages/orders/tests/orderList.spec.js
+++ packages/orders/tests/orderList.spec.js
@@ -1,11 +1,16 @@
 // eslint-disable-next-line no-undef
 const { test, expect } = require('@playwright/test')
 
-test('orders', async ({ page }) => {
+test('orders list', async ({ page }) => {
   await page.goto('/order')
 
   await expect(page).toHaveURL(/.*order/)
   await expect(page.getByText('Orders')).toBeVisible()
+  await expect(
+    page.getByRole('link', { name: 'Order from 4/18/2023' })
+  ).toBeVisible()
+  await expect(page.getByText('new')).toBeVisible()
+  await expect(page.getByRole('link', { name: 'Details' })).toBeVisible()
 })
 
 test.beforeEach(async ({ page, context }) => {
@@ -55,4 +60,64 @@ test.beforeEach(async ({ page, context }) => {
       json,
     })
   })
+
+  await page.route(
+    'http://localhost:8000/api/reservation/list',
+    async (route) => {
+      const json = [
+        {
+          id: 'fe41e987-ddcd-11ed-928e-0242ac150002',
+          branch: {},
+          createdAt: 1681811231,
+          notes: '',
+          books: [
+            {
+              id: 'af5e6199-dd0d-11ed-928e-0242ac150002',
+              branch: {},
+              added: 1681728636,
+              title: 'Demo Book 1',
+              shortDescription: null,
+              author: { id: 1, firstname: 'John', surname: 'Doe' },
+              genre: {
+                id: 1,
+                name: 'Crime',
+                branch: {},
+                books: 50,
+              },
+              price: 1,
+              sold: false,
+              soldOn: null,
+              removed: false,
+              removedOn: null,
+              reserved: true,
+              reservedAt: 1681811232,
+              releaseYear: 2023,
+              condition: null,
+              tags: [],
+              reservation_id: 'fe41e987-ddcd-11ed-928e-0242ac150002',
+              recommendation: false,
+              inventory: null,
+              format: null,
+              subtitle: null,
+              duplicate: false,
+            },
+          ],
+          salutation: 'w',
+          firstname: 'Jane',
+          surname: 'Doe',
+          mail: '',
+          phone: '',
+          open: true,
+        },
+      ]
+      await route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        json,
+      })
+    }
+  )
 })
