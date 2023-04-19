52739099d7dbd0e54ff95656576c4ee25a009838
André <a@baldeweg.org>
2023-04-19 12:13:16 +0000

    tests

--- packages/accounts/playwright.config.js
+++ packages/accounts/playwright.config.js
@@ -3,7 +3,7 @@ const { defineConfig, devices } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './tests',
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
