d07a241d2cb4ffabf96c84ffb5c4e790522e8f48
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-03-07 15:27:27 +0000

    Remove Cypress and add Playwright

---
+++ packages/settings/tests/format.spec.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-undef
+const { test, expect } = require('@playwright/test')
+
+test('format', async ({ page }) => {
+  await page.goto('/format')
+
+  await expect(page.getByText('This is your about text.')).toBeVisible()
+})
+
+test.beforeEach(async ({ page, context }) => {
+  // Cookies
+  const cookies = [
+    {
+      name: 'token',
+      value: 'token',
+      path: '/',
+      domain: 'localhost',
+    },
+    {
+      name: 'refresh_token',
+      value: 'refresh_token',
+      path: '/',
+      domain: 'localhost',
+    },
+  ]
+  await context.addCookies(cookies)
+
+  // API
+  await page.route('http://localhost:8000/api/me', async (route) => {
+    const json = {}
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      json,
+    })
+  })
+})
