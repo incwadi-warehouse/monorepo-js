9617a13fc750468bc65b3dd77d6b09a084e64464
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-03-23 20:52:08 +0000

    Fix test

--- packages/shop/tests/banner.spec.js
+++ packages/shop/tests/banner.spec.js
@@ -6,3 +6,22 @@ test('banner', async ({ page }) => {
 
   await expect(page.getByText('Our store will be closed')).toBeVisible()
 })
+
+test.beforeEach(async ({ page }) => {
+  await page.route('**/api/public/branch/show/**', async (route) => {
+    const json = {
+      id: 1,
+      name: 'Hamburg',
+      content:
+        '**Our store will be closed tomorrow for maintenance work.**\n\nWe are looking forward to provide you with the best service again the day after tomorrow. Thanks for being a loyal customer!',
+    }
+    await route.fulfill({
+      status: 200,
+      contentType: 'application/json',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      json,
+    })
+  })
+})
