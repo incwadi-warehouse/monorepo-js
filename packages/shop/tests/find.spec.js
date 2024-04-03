f9f3314c449b0a11c06952d3b00c4081cfeb1a0c
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-04-03 16:25:18 +0000

    Add tests

--- packages/shop/tests/find.spec.js
+++ packages/shop/tests/find.spec.js
@@ -1,22 +1,76 @@
 // eslint-disable-next-line no-undef
 const { test, expect } = require('@playwright/test')
 
-test('find', async ({ page }) => {
+test('search bar', async ({ page }) => {
   await page.goto('/find')
 
   await expect(
     page.getByPlaceholder('Search in Title, Author or')
   ).toBeVisible()
 
+  await page.getByPlaceholder('Search in Title, Author or').fill('test')
+
+  await page.getByRole('button').nth(1).click()
+
+  await expect(page).toHaveURL(/.*term=test.*/)
+
+  await page.locator('form').getByRole('button').first().click()
+
+  await expect(page).toHaveURL(/.*home$/)
+})
+
+test('genres', async ({ page }) => {
+  await page.goto('/find')
+
   await expect(page.getByText('Genres')).toBeVisible()
+
+  await page.getByText('Genres').click()
+
+  await page.getByText('Novels').click()
+
+  await expect(page).toHaveURL(/.*genre=1.*/)
+
+  await page.locator('form').getByRole('button').first().click()
+
+  await expect(page).toHaveURL(/.*home$/)
+})
+
+test('search results', async ({ page }) => {
+  await page.goto('/find')
 
   await page.getByRole('button').nth(1).click()
 
   await expect(page.getByText('Title 1')).toBeVisible()
 
+  await expect(page.locator('.list')).toHaveCount(23)
+
+  await page.locator('form').getByRole('button').first().click()
+
+  await expect(page).toHaveURL(/.*home$/)
+})
+
+test('pagination', async ({ page }) => {
+  await page.goto('/find')
+
   await page.getByText('Forward >').click()
 
   await expect(page).toHaveURL(/.*page=2.*/)
+
+  await page.getByText('< Back').click()
+
+  await expect(page).toHaveURL(/.*page=1.*/)
+
+  await page.getByText('2', { exact: true }).click()
+
+  await expect(page).toHaveURL(/.*page=2.*/)
+
+  await page.getByText('1', { exact: true }).click()
+
+  await expect(page).toHaveURL(/.*page=1.*/)
+
+  await page.locator('form').getByRole('button').first().click()
+
+  await expect(page).toHaveURL(/.*home$/)
 })
 
 test.beforeEach(async ({ page }) => {
