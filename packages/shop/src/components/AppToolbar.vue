f7443dbae5f6598ab84759b7c70b10ee59b0bc6a
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-04-05 12:48:03 +0000

    Content

--- packages/shop/src/components/AppToolbar.vue
+++ packages/shop/src/components/AppToolbar.vue
@@ -1,12 +1,42 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import SearchCheckboxFilter from '@/components/search/SearchCheckboxFilter.vue'
+import { useGenre } from '@/composables/useGenre.js'
+import Cart from '@/components/AppCart.vue'
+
+const router = useRouter()
+const { genres } = useGenre()
+
+const term = ref('')
+const genre = ref(null)
+
+const find = () => {
+  router.push({
+    name: 'find',
+    query: { term: term.value, genre: genre.value },
+  })
+}
+</script>
 
 <template>
   <div class="toolbar">
     <BContainer size="m">
       <div class="toolbar_inner">
-        <div class="toolbar_item">genres</div>
-        <div class="toolbar_item">loading</div>
-        <div class="toolbar_item">cart</div>
+        <div class="toolbar_item">
+          <SearchCheckboxFilter
+            :items="genres"
+            fieldKey="id"
+            fieldValue="name"
+            :title="$t('genres')"
+            v-model="genre"
+            @update:modelValue="find"
+          />
+        </div>
+
+        <div class="toolbar_item">
+          <Cart />
+        </div>
       </div>
     </BContainer>
   </div>
