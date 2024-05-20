923b545befc061af0f7131c7b68ce0fd04565fcc
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-05-20 11:28:10 +0000

    Rename Component

--- packages/shop/src/components/AppToolbar.vue
+++ packages/shop/src/components/AppToolbar.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import SearchCheckboxFilter from '@/components/search/SearchCheckboxFilter.vue'
+import SearchCheckboxFilter from '@/components/find/FindCheckboxFilter.vue'
 import { useGenre } from '@/composables/useGenre.js'
 import Cart from '@/components/AppCart.vue'
 
