d82f312f823c465463be8da7ec5e7c72a63b7ecb
André Baldeweg <56736413+abaldeweg@users.noreply.github.com>
2024-07-01 12:51:53 +0000

    esm

--- packages/orders/tests/orderList.spec.js
+++ packages/orders/tests/orderList.spec.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-undef
-const { test, expect } = require('@playwright/test')
+import { test, expect } from '@playwright/test'
 
 test('orders list', async ({ page }) => {
   await page.goto('/order')
